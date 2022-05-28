c9fef72c3e46db0ba302179b629d1f37bf63ad0f
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2022-05-28 07:56:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    if (sorted(s1) == sorted(s2)):
+        return True
+    else:
+        return False
+
+
+s1 = "listen"
+s2 = "silent"
+print(find_anagram(s1, s2))
+
+
+    # driver code
+
+
